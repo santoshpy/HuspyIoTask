4789c5814658ad9559d53a524de07d5597f8d461
Santosh Purbey <purbey.santosh@hotmail.com>
2021-06-10 19:51:35 +0545

    create models

--- graph/models.py
+++ graph/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Node(models.Model):
+    """
+    A graph is a way of specifying relationship among a set of objects,
+    called nodes connected by links called edges.
+
+    Two types graph:
+
+    1. an undirected graph (a <-> b <-> c <-> D)
+    2. a directed graph    (a -> b -> c -> D)
+
+    """
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Edge(models.Model):
+    """
+    we say that two nodes are neighbors if the are connected by an edge.
+
+    """
+    start_node = models.ForeignKey(
+        Node,
+        verbose_name="From",
+        related_name="as_start_edge",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    target_node = models.ForeignKey(
+        Node,
+        verbose_name="To",
+        related_name="as_target_edge",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    class Meta:
+        unique_together = (('start_node', 'target_node'),)
+
+    def __str__(self):
+        return f"{self.start_node} <-> {self.target_node}"
